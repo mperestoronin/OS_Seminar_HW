28c6c502e90fe810c02f478e85035c1a2e3bdaee
Maxim Perestoronin <116491226+mperestoronin@users.noreply.github.com>
2023-06-04 14:45:07 +0300

    Create client.c

---
+++ HW12/client.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT     8080
+#define MAXLINE 1024
+
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    struct sockaddr_in servaddr, cliaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int len, n;
+    len = sizeof(cliaddr);  
+
+while(1) {
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE - 1, 0, (struct sockaddr *) &cliaddr, &len);
+
+    if (n == -1) {
+        perror("recvfrom failed");
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[n] = '\0';
+    printf("Server : %s\n", buffer);
+}
+
+
+    return 0;
+}
